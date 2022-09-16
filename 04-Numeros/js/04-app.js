3205c223727b3555130526e1a6c72647b2462ac1
Jonathan Riaño <jodavidria80@gmail.com>
2022-09-16 10:49:20 -0500

    numeros

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,19 @@
+// El Orden en que se ejecutan las operaciones en JavaScript es igual que en la escuela
+
+
+let resultado 
+// El orden de las operaciones
+
+//primero multiplica
+resultado = 20 + 30 * 2;
+
+//primero suma
+resultado =  ( 20 + 30 ) * 2;
+
+// 20% De descuento en un carrito de compra.
+resultado = ( 20+10+30+40+50 ) * .20;
+
+//agregar iva o impuesto del 19%
+resultado = (40 + 80) * 1.19;
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,15 @@
+// Veamos como ir incrementando un valor de 1 en 1 o en determinada cantidad
+
+// Incluir incremento por 1 y menos 1
+
+let puntaje = 5;
+puntaje++; //retorna 5
+puntaje--; //retorna 5
+
+++puntaje; //retorna 6
+--puntaje; //retorna 4
+
+puntaje += 3; //retorna 8
+puntaje -= 3; //retorna 2
+
+console.log(puntaje);
